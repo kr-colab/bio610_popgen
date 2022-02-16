6d4604c8a334725eadb69d27291eb698ebd4438b
andrewkern <adkern@uoregon.edu>
2022-02-15 21:22:06 -0800

    may have tracked way too much stuff here..

---
+++ book/_build/jupyter_execute/test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[ ]:
+
+
+
+
+
+# 
+
+# # hello
+
+# x = np.random.ranom()
+
+# In[2]:
+
+
+x = np.random.random(100)
+y = np.random.random(100)
+plt.scatter(x, y)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
